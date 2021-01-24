2f62f57253d41aedf7be32be7d122c7b84dccf81
Oleg Upovalov <2987026@mail.ru>
2021-01-25 00:40:01 +0300

    completed all the tasks of lesson 2

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -19,7 +19,7 @@ from mainapp import views as mainapp_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', mainapp_views.index),
-    path('products/', mainapp_views.products),
+    path('', mainapp_views.index, name = 'index'),
+    path('products/', mainapp_views.products, name = 'products'),
 
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
+from json import load
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'title': 'geekshop',
+    }
+    return render(request, 'mainapp/index.html', context)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    with open("mainapp/fixtures/products.json", "r") as json_data:
+        catalog = load(json_data)
+        catalog_list = catalog.values()
+    context = {
+        'title': 'geekshop - каталог',
+        'products': catalog_list,
+    }
+    return render(request, 'mainapp/products.html', context)
